6ef031bfee34e9cc27eccdb42706dbc8acc330bb
caozicheng <407182090@qq.com>
2022-10-21 12:14:19 +0800

    roll-back secure-sum

--- suda-core/src/main/java/com/suda/federate/function/FederateQuery.java
+++ suda-core/src/main/java/com/suda/federate/function/FederateQuery.java
@@ -84,22 +84,26 @@ public abstract class FederateQuery {
                 .addAllEndpoints(endpoints)
                 .setResponse(FederateService.SummationResponse.newBuilder());
 
-        int parallelNum = 2;
-        List<FederateUtils.BoundPair> boundDivide = FederateUtils.boundDivide(parallelNum, siloSize);
-//        List<FederateService.SummationResponse> responseList = new ArrayList<>();
-
         List<List<Integer>> fakeLocalSumList = new ArrayList<>();
 
-        for (int i = 0; i < parallelNum; i++) {
-            // index = silo_id - 1
-            int start = boundDivide.get(i).start - 1;
-            int end = boundDivide.get(i).end - 1;
-            LogUtils.debug("start" + start + "end: " + end);
-            FederateDBClient client = federateDBClients.get(start);
-            FederateService.SummationResponse response = client.localSummation(requestBuilder.setNowIndex(start).setEndIndex(end).build());
-            // 会阻塞吗？
-            fakeLocalSumList.addAll(response.getFakeLocalSumList().stream().map(x -> x.getNumList()).collect(Collectors.toList()));
-        }
+       //  int parallelNum = 2;
+       //  List<FederateUtils.BoundPair> boundDivide = FederateUtils.boundDivide(parallelNum, siloSize);
+       // List<FederateService.SummationResponse> responseList = new ArrayList<>();
+       //  for (int i = 0; i < parallelNum; i++) {
+       //      index = silo_id - 1
+       //      int start = boundDivide.get(i).start - 1;
+       //      int end = boundDivide.get(i).end - 1;
+       //      LogUtils.debug("start" + start + "end: " + end);
+       //      FederateDBClient client = federateDBClients.get(start);
+       //      FederateService.SummationResponse response = client.localSummation(requestBuilder.setNowIndex(start).setEndIndex(end).build());
+       //      // 会阻塞吗？
+       //      fakeLocalSumList.addAll(response.getFakeLocalSumList().stream().map(x -> x.getNumList()).collect(Collectors.toList()));
+       //  }
+
+        int leaderIndex = 0;
+        FederateDBClient leaderClient = federateDBClients.get(leaderIndex);
+        FederateService.SummationResponse response = leaderClient.localSummation(requestBuilder.setNowIndex(leaderIndex).setEndIndex(federateDBClients.size() - 1).build());
+        fakeLocalSumList.addAll(response.getFakeLocalSumList().stream().map(x -> x.getNumList()).collect(Collectors.toList()));
 
         LogUtils.debug(fakeLocalSumList.toString());
         List<Integer> S = computeS(fakeLocalSumList);
@@ -107,7 +111,7 @@ public abstract class FederateQuery {
 
         int secureSum = lag(publicKeyList, S, 0);
         LogUtils.debug("privacy summation finished.");
-        //清理各silo垃圾
+        // 清理各silo垃圾
         clearCache(expression.getUuid());
         return secureSum;
     }
@@ -121,14 +125,14 @@ public abstract class FederateQuery {
         FederateDBClient leaderFederateDBClient = federateDBClients.get(leaderIndex);
         FederateService.UnionResponse unionResponse = leaderFederateDBClient.privacyUnion(unionRequest.build());
 //        System.out.println(unionResponse);
-        //清理各silo垃圾
+        // 清理各silo垃圾
         clearCache(expression.getUuid());
         return unionResponse.getPointList();
     }
 
     protected static void checkStatus(List<?> statusList, Class clazz) {
         boolean status = true;
-        //if(statusList.get(0))
+        // if(statusList.get(0))
         if (clazz == Boolean.class) {
             status = statusList.stream().allMatch(x -> (Boolean) x);
         } else if (clazz == Future.class) {
